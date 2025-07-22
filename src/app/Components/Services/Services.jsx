6151f1a7f63f4769027f7ffc12befc0866efdeeb
islamemon59 <islamemon59@gmail.com>
2025-07-22 20:53:55 +0600

    mongodb collection added data added form db

--- src/app/Components/Services/Services.jsx
+++ src/app/Components/Services/Services.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ServicesCard from "../ServicesCard/ServicesCard";
+import dbConnect from "@/lib/dbConnect";
 
 const Services = async () => {
-  const res = await fetch("http://localhost:3000/services.json");
-  const data = await res.json();
+  const carServiceItemCollection = dbConnect("carItems")
+  const data = await carServiceItemCollection.find({}).toArray()
   return (
-    <div className="grid grid-cols-12 gap-6">
+    <div className="grid grid-cols-12 gap-6 max-w-7xl mx-auto">
       {data.map((item) => (
         <ServicesCard key={item._id} item={item} />
       ))}
--- src/app/Components/ServicesCard/ServicesCard.jsx
+++ src/app/Components/ServicesCard/ServicesCard.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
-const ServicesCard = ({item}) => {
-    return (
-            <div className='col-span-4'>
-                <Image src={item.img} width={314} height={208} className='h-full' />
-            </div>
-    );
+const ServicesCard = ({ item }) => {
+  return (
+    <div className="col-span-4 p-5 border border-gray-300 rounded-lg">
+      <div className="h-52">
+        <Image
+          src={item.img}
+          width={314}
+          height={208}
+          className="h-full w-full rounded-lg"
+        />
+      </div>
+      <div className="mt-5">
+        <p className="text-2xl font-bold text-[#444444]">{item.title}</p>
+        <p className="text-xl font-semibold text-[#FF3811]">
+          price : ${item.price}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ServicesCard;
